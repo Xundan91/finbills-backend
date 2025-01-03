dcee938f4c14002197404dcd4d6e73196021ee2b
Tanishq971 <157120919+Tanishq971@users.noreply.github.com>
2025-01-03 16:44:59 +0530

    added inventory

--- utils/apiRoutes.ts
+++ utils/apiRoutes.ts
@@ -3,6 +3,9 @@ import express from "express";
 import businessRoutes from "../routes/business";
 =======
 import businessRoutes from "../routes/buisness";
+<<<<<<< HEAD
+>>>>>>> 560d77e94b706fab8706a74e8b7ab108c19798ea
+=======
 >>>>>>> 560d77e94b706fab8706a74e8b7ab108c19798ea
 import productRoutes from "../routes/product";
 

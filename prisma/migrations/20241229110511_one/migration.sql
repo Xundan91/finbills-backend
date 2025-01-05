83d9d6523629c87ea846e0c333914150c0bc7e8f
Xundan91 <kundansingh023230@gmail.com>
2025-01-05 10:17:12 +0530

    updated running schema

--- prisma/migrations/20241229110511_one/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ALTER COLUMN "alert_quantity" SET DEFAULT 0,
-ALTER COLUMN "mrp" SET DEFAULT 0,
-ALTER COLUMN "opening_stock" SET DEFAULT 0;
--- prisma/migrations/20241229112610_second/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ALTER COLUMN "gst_rate" SET DEFAULT 'ZERO',
-ALTER COLUMN "unit" SET DEFAULT 'PIECE';

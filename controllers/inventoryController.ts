afcd68a41f0552018ac6a9503c903404fccb1720
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-01-12 17:21:50 +0530

    upt: api's update

--- controllers/inventoryController.ts
+++ controllers/inventoryController.ts
@@ -78,17 +78,42 @@ export const fetchCategory = async (req: Request, res: Response) => {
 
 export const addProducts = async (req: Request, res: Response) => {
 	const productDetails: productSchema = req.body();
-	const businessId = req.headers.businessId;
+	const businessId = Number(req.headers.businessId);
 	const categoryId = req.params;
 	const validateProduct = zodProductSchema.safeParse(productDetails);
 
 	if (validateProduct.success) {
 		try {
-			const existingProduct = prisma.product.findUnique({
+			const allProduct = await prisma.product.findMany({
 				where: {
 					businessId,
-					categoryId,
 				},
+			});
+			allProduct.forEach(async (product) => {
+				if (product.itemName == validateProduct.data.itemName) {
+					res.json({
+						msg: "Item already exist",
+					});
+				} else {
+					const fullProductDetail = {
+						businessId,
+						...validateProduct.data,
+					};
+					const product = await prisma.product.create({
+						data: fullProductDetail,
+					});
+
+					if (product) {
+						res.json({
+							msg: "Sucess",
+							product,
+						});
+					} else {
+						res.status(411).json({
+							msg: "Product failed to add",
+						});
+					}
+				}
 			});
 		} catch (err) {
 			console.log(`Error: ${err}`);
@@ -97,6 +122,31 @@ export const addProducts = async (req: Request, res: Response) => {
 	} else {
 		res.status(411).json({
 			msg: "Failed! Validation issue",
+		});
+	}
+};
+
+export const getProducts = async (req: Request, res: Response) => {
+	const businessId = Number(req.headers.businessId);
+	try {
+		const allProducts = await prisma.product.findMany({
+			where: {
+				businessId,
+			},
+		});
+		if (allProducts) {
+			res.json({
+				msg: "Sucess",
+				allProducts,
+			});
+		} else {
+			res.status(411).json({
+				msg: "Failed",
+			});
+		}
+	} catch (err) {
+		res.status(500).json({
+			msg: `Error: ${err}`,
 		});
 	}
 };
